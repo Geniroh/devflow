06942c05ac3c5c627a062274ee50c16e31c5b322
Geniroh <irochibuzor@gmail.com>
2024-12-30 13:31:07 +0100

    eslint and prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl">Hello World</h1>
+      <p className="text-lg">Welcome to Next.js!</p>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </div>
   );
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,48 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "standard", "plugin:tailwindcss/recommended", "prettier"),
+  ...compat.rules({ 
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "builtin", // Built-in types are first
+          "external", // External libraries
+          "internal", // Internal modules
+          [
+            "parent",
+            "sibling"
+          ], // Parent and sibling types can be mingled together
+          "index", // Then the index file
+          "object" // Object imports
+        ],
+        "newlines-between": "always",
+        "pathGroups": [
+          {
+            "pattern": "@app/**",
+            "group": "external",
+            "position": "after"
+          }
+        ],
+        "pathGroupsExcludedImportTypes": [
+          "builtin"
+        ],
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ]
+  }),
+  ...compat.plugins("import"),
+  ...compat.ignorePatterns("components/ui/**"),
+  ...compat.overrides({
+    files: ["*.ts", "*.tsx"],
+    rules: {
+      "no-undef": "off"
+    }
+  })
 ];
 
 export default eslintConfig;
