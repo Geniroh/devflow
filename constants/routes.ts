e469fe5d0d2d6f3ada1e1d48c6e4d674325114b6
Geniroh2 <chibuzor@codematic.io>
2025-01-02 10:55:09 +0100

    search bar

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,6 +2,7 @@ interface Routes {
   HOME: string;
   SIGN_IN: string;
   SIGN_UP: string;
+  ASK_QUESTION: string;
   PROFILE: <T extends string | number>(id: T) => string;
   TAGS: <T extends string | number>(id: T) => string;
 }
@@ -10,6 +11,7 @@ const ROUTES: Routes = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
+  ASK_QUESTION: "/ask-question",
   PROFILE: (id) => `/profile/${id}`,
   TAGS: (id) => `/tags/${id}`,
 };
